611b32b0c90e854a323e95017cf9cba75a321ed3
oaky <federicorojkin@gmail.com>
2013-07-25 18:53:57 -0300

    2do

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rosario::Application.config.secret_key_base = '5925eefffda1037a1ca85a9d980673aa4fc52a99617b45db9728ee19fdb58420db42aa3f4eed23d4d2eb7a03dc4cabc5196f7fdd91217fbc91e809071e7bfa1b'
